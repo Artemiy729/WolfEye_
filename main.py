80ca15b21f22d73b40fa805c6f7edc26f8ce4b79
Megadeth2006 <danielgrigorevdlol@gmail.com>
2025-09-06 13:17:52 +0300

    changed prompt for FIO

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ import time
 
 def main():
     
-    start = time.perf_counter()
-    res1 = analysis_fio(NameParts(surname="Смирнов", name="Александр", father_name="Вячеславович"))
-    end = time.perf_counter()
-    print(res1, "вероятность:" + str(res1.suspicion_score()), f"(заняло {end - start:.3f} сек)")
+    # start = time.perf_counter()
+    # res1 = analysis_fio(NameParts(surname="Смирнов", name="Александр", father_name="Вячеславович"))
+    # end = time.perf_counter()
+    # print(res1, "вероятность:" + str(res1.suspicion_score()), f"(заняло {end - start:.3f} сек)")
 
 
     start = time.perf_counter()
-    res2 = analysis_fio(NameParts(surname="Пидоров", name="Иван", father_name="Ивaнович"))
+    res2 = analysis_fio(NameParts(surname="Сидр", name="Иван", father_name="Иванович"))
     end = time.perf_counter()
-    print(res2, "вероятность" + str(res2.suspicion_score()), f"(заняло {end - start:.3f} сек)")
+    print(res2, "вероятность:" + str(res2.suspicion_score()), f"(заняло {end - start:.3f} сек)")
 
-    start = time.perf_counter()
-    res3 = analysis_fio(NameParts(surname="Ф)(торович", name="Иван", father_name="Математикович"))
-    end = time.perf_counter()
-    print(res3, "вероятность" + str(res3.suspicion_score()), f"(заняло {end - start:.3f} сек)")
+    # start = time.perf_counter()
+    # res3 = analysis_fio(NameParts(surname="Ф)торович", name="Иван", father_name="Математикович"))
+    # end = time.perf_counter()
+    # print(res3, "вероятность:" + str(res3.suspicion_score()), f"(заняло {end - start:.3f} сек)")
     
     
     # core = CoreML()
